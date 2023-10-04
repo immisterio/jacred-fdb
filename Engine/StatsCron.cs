0e60362614d5871ea4a2142e31195ec367edfa9a
immisterio <96652250+immisterio@users.noreply.github.com>
2023-10-04 11:47:32 +0300

    fix

--- Engine/StatsCron.cs
+++ Engine/StatsCron.cs
@@ -18,7 +18,7 @@ namespace JacRed.Engine
                 try
                 {
                     var today = DateTime.Today - (DateTime.Now - DateTime.UtcNow);
-                    var stats = new Dictionary<string, (DateTime lastnewtor, int newtor, int update, int check, int alltorrents, int trkconfirm, int trkwait)>();
+                    var stats = new Dictionary<string, (DateTime lastnewtor, int newtor, int update, int check, int alltorrents, int trkconfirm, int trkwait, int trerror)>();
 
                     foreach (var item in FileDB.masterDb.ToArray())
                     {
@@ -30,7 +30,7 @@ namespace JacRed.Engine
                             try
                             {
                                 if (!stats.TryGetValue(t.trackerName, out var val))
-                                    stats.Add(t.trackerName, (t.createTime, 0, 0, 0, 0, 0, 0));
+                                    stats.Add(t.trackerName, (t.createTime, 0, 0, 0, 0, 0, 0, 0));
 
                                 var s = stats[t.trackerName];
                                 s.alltorrents = s.alltorrents + 1;
@@ -47,9 +47,11 @@ namespace JacRed.Engine
                                 if (t.checkTime >= today)
                                     s.check = s.check + 1;
 
-                                if (!TracksDB.theBad(t.types) && !string.IsNullOrEmpty(t.magnet) && t.ffprobe_tryingdata < 3)
+                                if (!TracksDB.theBad(t.types) && !string.IsNullOrEmpty(t.magnet))
                                 {
-                                    if (TracksDB.Get(t.magnet) != null || t.ffprobe != null)
+                                    if (t.ffprobe_tryingdata >= 3)
+                                        s.trerror = s.trerror + 1;
+                                    else if (TracksDB.Get(t.magnet) != null || t.ffprobe != null)
                                         s.trkconfirm = s.trkconfirm + 1;
                                     else
                                         s.trkwait = s.trkwait + 1;
@@ -72,7 +74,8 @@ namespace JacRed.Engine
                         tracks = new 
                         {
                             wait = i.Value.trkwait,
-                            confirm = i.Value.trkconfirm
+                            confirm = i.Value.trkconfirm,
+                            skip = i.Value.trerror
                         }
 
                     }), Formatting.Indented));
